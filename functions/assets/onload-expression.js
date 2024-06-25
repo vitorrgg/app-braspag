e20db84c6206f3ba00eb999b963379be2ad4e9bf
Wisley Alves <wisley7lgithub@gmail.com>
2024-06-25 19:54:51 -0300

    fix(onload-expression): test scripts clearSales

--- functions/assets/onload-expression.js
+++ functions/assets/onload-expression.js
@@ -3,17 +3,30 @@
   console.log('>> SOP: ', isSandbox)
 
   const accessToken = window._braspagAccessToken
-  const elementScript = 'var io_bbout_element_id = "gatewayFingerprint";'
+  const elementScript = `(function (a, b, c, d, e, f, g) {
+    a['CsdpObject'] = e; a[e] = a[e] || function () {
+    (a[e].q = a[e].q || []).push(arguments)
+    }, a[e].l = 1 * new Date(); f = b.createElement(c),
+    g = b.getElementsByTagName(c)[0]; f.async = 1; f.src = d; g.parentNode.insertBefore(f, g)
+    })(window, document, 'script', '//device.clearsale.com.br/p/fp.js', 'csdp');
+    csdp('app', 'seu_app');
+    csdp('outputsessionid', 'MeuCampoQueReceberaValor');`
+
   const newScript = document.createElement('script')
   newScript.innerHTML = elementScript
   document.head.appendChild(newScript)
   // const merchantId = window._braspagMerchantIdProtectedCard
-  const newScript2 = document.createElement('script')
-  newScript2.type = 'text/javascript'
-  newScript2.src = 'https://mpsnare.iesnare.com/snare.js'
-  document.head.append(newScript2)
+  // const newScript2 = document.createElement('script')
+  // newScript2.type = 'text/javascript'
+  // newScript2.src = 'https://mpsnare.iesnare.com/snare.js'
+  // document.head.append(newScript2)
 
-  const elementsScript = '<input type="hidden" name="gatewayFingerprint" id="gatewayFingerprint"> </input>'
+  // const elementsScript = '<input type="hidden" name="gatewayFingerprint" id="gatewayFingerprint"> </input>'
+  // const newForm = document.createElement('form')
+  // newForm.innerHTML = elementsScript
+  // document.body.appendChild(newForm)
+
+  const elementsScript = '<input type="hidden" id="MeuCampoQueReceberaValor" value=""/>'
   const newForm = document.createElement('form')
   newForm.innerHTML = elementsScript
   document.body.appendChild(newForm)
